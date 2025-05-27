e98c4a6a6bb3243227616174c2cb25fb63cf37a1
nguyenngocuyenchi <152564377+nguyenngocuyenchi@users.noreply.github.com>
2025-05-27 11:18:50 +0200

    Delete exception directory

--- exception/AlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.DataSample.exception;
-
-public class AlreadyExistsException extends RuntimeException {
-
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
